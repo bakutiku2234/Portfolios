eb788f3f5da76534df53889ffcce5baa27269a60
bakutiku2234 <39401492+bakutiku2234@users.noreply.github.com>
2018-11-07 17:05:22 +0900

    change test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,95 +1,45 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <myheader></myheader>
+    <p v-if="msg.length > 0" >
+      {{ msg }}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg" />
+    <button @click="clear()">clear</button>
+    <button @click="revert()">revert</button>
   </div>
 </template>
 
 <script>
+import myheader from './myheader'
+
 export default {
   name: 'HelloWorld',
+  components: {
+    myheader
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  methods: {
+    clear () {
+      this.msg = ''
+    },
+    revert () {
+      this.msg = 'Welcome to Your Vue.js App'
+    }
+  },
+  created () {
+    var that = this
+    $.getJSON('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US&callback=?', {}, function (json) {
+      console.log(json)
+      that.msg = json.postalcodes[0].adminName1
+    })
   }
 }
 </script>
